593062a3cb022a5799b34773a900ffd89b53cfbc
SuperPorff2000 <superporff2000@gmail.com>
2023-12-11 10:22:36 +0530

    Idk what I did. My mind is going numb.
    Seriously though college classroom is worst place for development.

--- src/main.c
+++ src/main.c
@@ -1,53 +1,107 @@
 #include <SDL2/SDL.h>
-#include <stdio.h>
-
-#define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 720
 
 int main(int argc, char **argv)
 {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
+    SDL_Window *window = NULL;
+    window = SDL_CreateWindow(
+        "Jeu de la vie", SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        SDL_WINDOW_SHOWN);
 
-    SDL_Window *window = SDL_CreateWindow("SDL test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-    if (!window)
-    {
-        printf("Error: Failed to open window\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
+    // Setup renderer
+    SDL_Renderer *renderer = NULL;
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer)
-    {
-        printf("Error: Failed to create renderer\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
+    // Set render color to red ( background will be rendered in this color )
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 
-    int running = 1;
-    while (running)
+    // Clear window
+    SDL_RenderClear(renderer);
+
+    // Main tile
+    SDL_Rect mainTile;
+    mainTile.x = 220;
+    mainTile.y = 400;
+    mainTile.w = 200;
+    mainTile.h = 25;
+
+    // Set render color to blue ( rect will be rendered in this color )
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+
+    // Define tile properties
+    int tile_width = 100;
+    int tile_height = 25;
+    int gap = 5; // gap between tiles
+
+    // Game loop flag
+    int quit = 0;
+
+    // Event handler
+    SDL_Event e;
+
+    while (!quit)
     {
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
+        // Handle events on the queue
+        while (SDL_PollEvent(&e) != 0)
         {
-            switch (event.type)
+            // User requests quit
+            if (e.type == SDL_QUIT)
             {
-            case SDL_QUIT:
-                running = 0;
-                break;
-
-            default:
-                break;
+                quit = 1;
+            }
+            // Handle key presses
+            else if (e.type == SDL_KEYDOWN)
+            {
+                switch (e.key.keysym.sym)
+                {
+                case SDLK_a:
+                    // Move the main tile to the left
+                    mainTile.x -= 10;
+                    break;
+                case SDLK_d:
+                    // Move the main tile to the right
+                    mainTile.x += 10;
+                    break;
+                }
             }
         }
 
-        SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+        // Clear the renderer
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderClear(renderer);
 
+        // Render the breakable blocks
+        for (int row = 0; row < 3; ++row)
+        {
+            for (int col = 0; col < 8; ++col)
+            {
+                SDL_Rect tile;
+                tile.x = col * (tile_width + gap);
+                tile.y = row * (tile_height + gap);
+                tile.w = tile_width;
+                tile.h = tile_height;
+
+                // Render rect
+                SDL_RenderFillRect(renderer, &tile);
+            }
+        }
+
+        // Render rect
+        SDL_RenderFillRect(renderer, &mainTile);
+
+        // Render the rect to the screen
         SDL_RenderPresent(renderer);
+
+        // Wait for a short period (adjust as needed)
+        SDL_Delay(10);
     }
 
-    return 0;
+    // Cleanup and quit SDL
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
